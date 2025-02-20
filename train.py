08dacaaf8a76a8baffc07531d177dc34c2bcf1ba
remi.brenaut <remi.brenaut@epita.fr>
2025-02-20 17:52:28 +0100

    feat: neural network model

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import json
-import nltk_utils
 import numpy as np
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from nltk_utils import *
+
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -27,7 +28,6 @@ ignore_words = ['?', '!', ',', '.']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
 
 X_train = []
 y_train = []
@@ -57,6 +57,37 @@ class ChatDataset(Dataset):
 
 #Hyperparameters
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0]) # len(all_words)
+learning_rate = 0.001
+num_epochs = 1000
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        #forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        #backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1) % 100 == 0:
+        print(f'epoch {epoch + 1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
